0c1a56f8e85d8d9ad79569606e40c0d6365d028b
Leo C <8117leo@gmail.com>
2022-02-26 17:30:53 -0500

    app

--- app03/views.py
+++ app03/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home2.html', {"info": "my name is Sean, I like python, this is step 2"})
+    return render(request, 'home2.html', {"info": "my name is Leo, I like python, this is step 2"})q
